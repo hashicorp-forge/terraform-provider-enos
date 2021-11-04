7b1d48bfb950ccced8aface6c64c7b65a90275e0
Rebecca Willett <47540675+rebwill@users.noreply.github.com>
2021-11-04 13:41:31 -0600

    implemented Retry functionality on remoteflight platform and architecture detection functions (#58)

--- internal/flightcontrol/remoteflight/remoteflight.go
+++ internal/flightcontrol/remoteflight/remoteflight.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
+	"github.com/hashicorp/enos-provider/internal/retry"
 	"github.com/hashicorp/enos-provider/internal/transport"
 	"github.com/hashicorp/enos-provider/internal/transport/command"
 )
@@ -13,22 +15,66 @@ import (
 func TargetPlatform(ctx context.Context, ssh transport.Transport) (string, error) {
 	// Get the platform and architecture of the remote machine so that we can
 	// make sure it's a supported target and so we can install the correct binary.
-	platform, _, err := ssh.Run(ctx, command.New("uname -s"))
-	if err != nil {
-		return "", fmt.Errorf("determining target host platform: %w", err)
+	getPlatform := func(ctx context.Context) (interface{}, error) {
+		platform, _, err := ssh.Run(ctx, command.New("uname -s"))
+		if err != nil {
+			return "", fmt.Errorf("determining target host platform: %w", err)
+		}
+
+		if platform == "" {
+			return "", fmt.Errorf("failed to determine platform")
+		}
+
+		return platform, nil
 	}
 
-	return formatPlatform(platform), nil
+	r, err := retry.NewRetrier(
+		retry.WithMaxRetries(5),
+		retry.WithIntervalFunc(retry.IntervalFibonacci(time.Second)),
+		retry.WithRetrierFunc(getPlatform),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	platform, err := r.Run(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return formatPlatform(platform.(string)), nil
 }
 
 // TargetArchitecture is a helper that determines the targets architecture
 func TargetArchitecture(ctx context.Context, ssh transport.Transport) (string, error) {
-	arch, _, err := ssh.Run(ctx, command.New("uname -m"))
-	if err != nil {
-		return "", fmt.Errorf("determining target host architecture: %w", err)
+	getArchitecture := func(ctx context.Context) (interface{}, error) {
+		arch, _, err := ssh.Run(ctx, command.New("uname -m"))
+		if err != nil {
+			return "", fmt.Errorf("determining target host architecture: %w", err)
+		}
+
+		if arch == "" {
+			return "", fmt.Errorf("failed to determine architecture")
+		}
+
+		return arch, nil
 	}
 
-	return formatArch(arch), nil
+	r, err := retry.NewRetrier(
+		retry.WithMaxRetries(5),
+		retry.WithIntervalFunc(retry.IntervalFibonacci(time.Second)),
+		retry.WithRetrierFunc(getArchitecture),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	arch, err := r.Run(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return formatArch(arch.(string)), nil
 }
 
 func formatPlatform(platform string) string {
