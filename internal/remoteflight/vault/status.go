663ae73fb4b79d3b8f7ad549e898c8f6d3137ac6
Mike Baum <mike.baum@hashicorp.com>
2022-12-08 13:24:32 -0500

    [QT-448] Update the Vault GetStatus and WaitForState functions to properly report errors (#144)

--- internal/remoteflight/vault/status.go
+++ internal/remoteflight/vault/status.go
@@ -48,6 +48,22 @@ func (s SealStatus) String() string {
 	return "StatusUnknown"
 }
 
+// FromExitCode gets the SealStatus from the exit code of the 'vault status' command.
+func FromExitCode(exitCode int) SealStatus {
+	switch exitCode {
+	case 0:
+		return UnSealed
+	case 1:
+		return Error
+	case 2:
+		return Sealed
+	case 9:
+		return StatusUnknown
+	default:
+		return StatusUnknown
+	}
+}
+
 func (i InitStatus) String() string {
 	switch i {
 	case Inactive:
@@ -82,30 +98,42 @@ func NewState() State {
 	}
 }
 
-type StateCheck func(s State) bool
+type StateCheck func(s State) error
 
 func CheckIsUnsealed() StateCheck {
-	return func(s State) bool {
-		return !s.Sealed
+	return func(s State) error {
+		if s.Sealed {
+			return fmt.Errorf("expected Vault to be unsealed, state: %#v", s)
+		}
+		return nil
 	}
 }
 
 func CheckIsActive() StateCheck {
-	return func(s State) bool {
-		return s.InitStatus != Inactive
+	return func(s State) error {
+		if s.InitStatus == Inactive {
+			return fmt.Errorf("expected Vault to be active, state: %#v", s)
+		}
+		return nil
 	}
 }
 
 func CheckIsInitialized() StateCheck {
-	return func(s State) bool {
-		return s.Initialized
+	return func(s State) error {
+		if !s.Initialized {
+			return fmt.Errorf("expected Vault to be initialized, state: %#v", s)
+		}
+		return nil
 	}
 }
 
 // CheckSealStatusKnown returns true if the seal status is either Sealed or Unsealed
 func CheckSealStatusKnown() StateCheck {
-	return func(s State) bool {
-		return s.SealStatus != StatusUnknown
+	return func(s State) error {
+		if s.SealStatus == StatusUnknown {
+			return fmt.Errorf("expected Vault seal status to be known, state: %#v", s)
+		}
+		return nil
 	}
 }
 
@@ -148,63 +176,79 @@ func WithStatusRequestVaultAddr(addr string) StatusRequestOpt {
 }
 
 // GetState returns the vault state
-func GetState(ctx context.Context, ssh it.Transport, req *StatusRequest) (State, error) {
-	state := NewState()
+func GetState(ctx context.Context, ssh it.Transport, req *StatusRequest) (*State, error) {
 	if req.BinPath == "" {
-		return state, fmt.Errorf("you must supply a vault bin path")
+		return nil, fmt.Errorf("you must supply a vault bin path")
 	}
 	if req.VaultAddr == "" {
-		return state, fmt.Errorf("you must supply a vault listen address")
+		return nil, fmt.Errorf("you must supply a vault listen address")
 	}
 
 	stdout, stderr, err := ssh.Run(ctx, command.New(
 		fmt.Sprintf("%s status -format=json", req.BinPath),
 		command.WithEnvVar("VAULT_ADDR", req.VaultAddr),
 	))
+	sealStatus := UnSealed
 	// If we get an error check the error code and get the seal status
-	if err != nil {
+	if err != nil || stderr != "" {
 		var exitError *it.ExecError
 		if errors.As(err, &exitError) {
-			state.SealStatus = SealStatus(exitError.ExitCode())
+			sealStatus = FromExitCode(exitError.ExitCode())
+			if sealStatus == Error || sealStatus == StatusUnknown {
+				return nil, remoteflight.WrapErrorWith(
+					fmt.Errorf("failed to execute status command, due to: %w", exitError),
+					createStdErrMessage(stderr),
+				)
+			}
 		} else {
-			return state, remoteflight.WrapErrorWith(fmt.Errorf("failed to execute status command, due to: %w", err), stderr)
+			return nil, remoteflight.WrapErrorWith(
+				fmt.Errorf("failed to execute status command, due to: %w", err),
+				createStdErrMessage(stderr),
+			)
 		}
-	} else {
-		// if there's no error the exec returned with a zero exit code, therefore the state is unsealed
-		state.SealStatus = UnSealed
-		state.InitStatus = Initialized
 	}
+
+	state := NewState()
+	state.SealStatus = sealStatus
 
 	err = json.Unmarshal([]byte(stdout), &state)
 	if err != nil {
 		tflog.Error(ctx, "Failed to unmarshal seal status", map[string]interface{}{"state": stdout})
-		return state, remoteflight.WrapErrorWith(fmt.Errorf("failed to unmarshal the seal status, due to: %w", err), stderr)
+		return nil, remoteflight.WrapErrorWith(
+			fmt.Errorf("failed to unmarshal the seal status: [%s], due to: %w", stdout, err),
+			createStdErrMessage(stderr),
+		)
 	}
+
 	if state.Initialized {
 		state.InitStatus = Initialized
 	} else {
 		state.InitStatus = Uninitialized
 	}
 
-	return state, nil
+	return &state, nil
+}
+
+func createStdErrMessage(stderr string) string {
+	if stderr == "" {
+		return ""
+	}
+	return fmt.Sprintf("stderr: [%s]", stderr)
 }
 
 // WaitForState waits until the vault service state satisfies all of the provided StateCheck(s).
 // If the context has a duration we will keep trying until it is done.
-func WaitForState(ctx context.Context, ssh it.Transport, req *StatusRequest, checks ...StateCheck) (State, error) {
-	state := NewState()
-	if len(checks) == 0 {
-		return state, nil
-	}
+func WaitForState(ctx context.Context, ssh it.Transport, req *StatusRequest, checks ...StateCheck) (*State, error) {
 
 	var err error
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
 
 	for {
+		var state *State
 		select {
 		case <-ctx.Done():
-			return state, fmt.Errorf("timed out waiting for vault: %w: state: %+v", ctx.Err(), state)
+			return state, fmt.Errorf("timed out waiting for vault: %w: state: %+v", err, state)
 		case <-ticker.C:
 			state, err = GetState(ctx, ssh, req)
 			tflog.Debug(ctx, "Checking Vault state", map[string]interface{}{
@@ -212,14 +256,20 @@ func WaitForState(ctx context.Context, ssh it.Transport, req *StatusRequest, che
 				"state":    fmt.Sprintf("%+v", state),
 			})
 			if err == nil {
-				checksOk := false
+				if len(checks) == 0 {
+					return state, nil
+				}
+				if state == nil {
+					continue
+				}
+
 				for _, check := range checks {
-					checksOk = checksOk || check(state)
-					if checksOk {
+					err = check(*state)
+					if err != nil {
 						break
 					}
 				}
-				if !checksOk {
+				if err != nil {
 					continue
 				}
 				tflog.Debug(ctx, "Vault state check done", map[string]interface{}{
