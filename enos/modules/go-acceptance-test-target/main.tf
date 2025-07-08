5e9fa36e820ac23b506f4b45bf0d8da502704015
kelly <69541941+kporter101@users.noreply.github.com>
2025-07-08 17:45:25 -0400

    updated ubuntu version in go acc test target

--- enos/modules/go-acceptance-test-target/main.tf
+++ enos/modules/go-acceptance-test-target/main.tf
@@ -22,7 +22,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-*-server-*"]
   }
 
   filter {
