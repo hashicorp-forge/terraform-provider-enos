71013dd0e1f05bc9a4dd7f38a6c25b9137758a95
Ryan Cragun <me@ryan.ec>
2026-06-19 12:47:27 -0600

    use the correct aws intance resource name
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- enos/modules/aws_ssh_vault_cluster/vault-instances.tf
+++ enos/modules/aws_ssh_vault_cluster/vault-instances.tf
@@ -21,9 +21,9 @@ resource "aws_instance" "vault_instance" {
 
 locals {
   hosts = { for idx in range(var.instance_count) : idx => {
-    ipv6       = try(aws_instance.targets[idx].ipv6_addresses[0], "")
-    public_ip  = aws_instance.targets[idx].public_ip
-    private_ip = aws_instance.targets[idx].private_ip
+    ipv6       = try(aws_instance.vault_instance[idx].ipv6_addresses[0], "")
+    public_ip  = aws_instance.vault_instance[idx].public_ip
+    private_ip = aws_instance.vault_instance[idx].private_ip
     }
   }
 }
