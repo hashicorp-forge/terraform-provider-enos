6dbadd0c628331abdb004374d6284f0b8405216e
tin.vo <tin.vo@hashicorp.com>
2026-09-11 13:23:54 -0700

    fixing lint

--- internal/plugin/tftypes_test.go
+++ internal/plugin/tftypes_test.go
@@ -94,14 +94,14 @@ func TestTFNumGetAndValue(t *testing.T) {
 		},
 		{
 			"null",
-			&tfNum{Null: true, Val: 4},
+			&tfNum{Unknown: false, Null: true, Val: 4},
 			tftypes.NewValue(tftypes.Number, nil),
 			4,
 			false,
 		},
 		{
 			"known",
-			&tfNum{Val: 5},
+			&tfNum{Unknown: false, Null: false, Val: 5},
 			tftypes.NewValue(tftypes.Number, 5),
 			5,
 			true,
@@ -129,21 +129,21 @@ func TestTFStringGetAndValue(t *testing.T) {
 	}{
 		{
 			"unknown",
-			&tfString{Unknown: true, Val: "1"},
+			&tfString{Unknown: true, Null: false, Val: "1"},
 			tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
 			"1",
 			false,
 		},
 		{
 			"null",
-			&tfString{Null: true, Val: "2"},
+			&tfString{Unknown: false, Null: true, Val: "2"},
 			tftypes.NewValue(tftypes.String, nil),
 			"2",
 			false,
 		},
 		{
 			"known",
-			&tfString{Val: "5"},
+			&tfString{Unknown: false, Null: false, Val: "5"},
 			tftypes.NewValue(tftypes.String, "5"),
 			"5",
 			true,
@@ -172,21 +172,21 @@ func TestTFStringSliceGetAndValue(t *testing.T) {
 	}{
 		{
 			"unknown",
-			&tfStringSlice{Unknown: true, Val: []*tfString{{Val: "foo"}}},
+			&tfStringSlice{Unknown: true, Null: false, Val: []*tfString{{Unknown: false, Null: false, Val: "foo"}}},
 			tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, tftypes.UnknownValue),
 			[]string{},
 			false,
 		},
 		{
 			"null",
-			&tfStringSlice{Null: true, Val: []*tfString{{Val: "foo"}}},
+			&tfStringSlice{Unknown: false, Null: true, Val: []*tfString{{Unknown: false, Null: false, Val: "foo"}}},
 			tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, nil),
 			[]string{},
 			false,
 		},
 		{
 			"fully known",
-			&tfStringSlice{Val: []*tfString{{Val: "foo"}, {Val: "bar"}}},
+			&tfStringSlice{Unknown: false, Null: false, Val: []*tfString{{Unknown: false, Null: false, Val: "foo"}, {Unknown: false, Null: false, Val: "bar"}}},
 			tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{
 				tftypes.NewValue(tftypes.String, "foo"),
 				tftypes.NewValue(tftypes.String, "bar"),
@@ -196,7 +196,7 @@ func TestTFStringSliceGetAndValue(t *testing.T) {
 		},
 		{
 			"known with unknown child",
-			&tfStringSlice{Val: []*tfString{{Unknown: true}}},
+			&tfStringSlice{Unknown: false, Null: false, Val: []*tfString{{Unknown: true, Null: false, Val: ""}}},
 			tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{
 				tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
 			}),
@@ -226,21 +226,21 @@ func TestTFStringMapGetAndValue(t *testing.T) {
 	}{
 		{
 			"unknown",
-			&tfStringMap{Unknown: true, Val: map[string]*tfString{"foo": {Val: "foo"}}},
+			&tfStringMap{Unknown: true, Null: false, Val: map[string]*tfString{"foo": {Unknown: false, Null: false, Val: "foo"}}},
 			tftypes.NewValue(tftypes.Map{ElementType: tftypes.String}, tftypes.UnknownValue),
 			map[string]string{},
 			false,
 		},
 		{
 			"null",
-			&tfStringMap{Null: true, Val: map[string]*tfString{"foo": {Val: "foo"}}},
+			&tfStringMap{Unknown: false, Null: true, Val: map[string]*tfString{"foo": {Unknown: false, Null: false, Val: "foo"}}},
 			tftypes.NewValue(tftypes.Map{ElementType: tftypes.String}, nil),
 			map[string]string{},
 			false,
 		},
 		{
 			"fully known",
-			&tfStringMap{Val: map[string]*tfString{"foo": {Val: "foo"}, "bar": {Val: "bar"}}},
+			&tfStringMap{Unknown: false, Null: false, Val: map[string]*tfString{"foo": {Unknown: false, Null: false, Val: "foo"}, "bar": {Unknown: false, Null: false, Val: "bar"}}},
 			tftypes.NewValue(tftypes.Map{ElementType: tftypes.String}, map[string]tftypes.Value{
 				"foo": tftypes.NewValue(tftypes.String, "foo"),
 				"bar": tftypes.NewValue(tftypes.String, "bar"),
@@ -250,7 +250,7 @@ func TestTFStringMapGetAndValue(t *testing.T) {
 		},
 		{
 			"known with unknown child",
-			&tfStringMap{Val: map[string]*tfString{"foo": {Unknown: true}}},
+			&tfStringMap{Unknown: false, Null: false, Val: map[string]*tfString{"foo": {Unknown: true, Null: false, Val: ""}}},
 			tftypes.NewValue(tftypes.Map{ElementType: tftypes.String}, map[string]tftypes.Value{
 				"foo": tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
 			}),
@@ -282,7 +282,7 @@ func TestTFObjectGetAndValue(t *testing.T) {
 			"unknown",
 			func() *tfObject {
 				obj := newTfObject()
-				obj.Set(map[string]any{"foo": &tfString{Val: "foo"}, "bar": &tfBool{Val: false}})
+				obj.Set(map[string]any{"foo": &tfString{Unknown: false, Null: false, Val: "foo"}, "bar": &tfBool{Unknown: false, Null: false, Val: false}})
 				obj.Unknown = true
 
 				return obj
@@ -300,7 +300,7 @@ func TestTFObjectGetAndValue(t *testing.T) {
 			"null",
 			func() *tfObject {
 				obj := newTfObject()
-				obj.Set(map[string]any{"foo": &tfString{Val: "foo"}, "bar": &tfBool{Val: false}})
+				obj.Set(map[string]any{"foo": &tfString{Unknown: false, Null: false, Val: "foo"}, "bar": &tfBool{Unknown: false, Null: false, Val: false}})
 				obj.Null = true
 
 				return obj
@@ -318,7 +318,7 @@ func TestTFObjectGetAndValue(t *testing.T) {
 			"fully known",
 			func() *tfObject {
 				obj := newTfObject()
-				obj.Set(map[string]any{"foo": &tfString{Val: "foo"}, "bar": &tfBool{Val: false}})
+				obj.Set(map[string]any{"foo": &tfString{Unknown: false, Null: false, Val: "foo"}, "bar": &tfBool{Unknown: false, Null: false, Val: false}})
 
 				return obj
 			},
@@ -338,7 +338,7 @@ func TestTFObjectGetAndValue(t *testing.T) {
 			"known with unknown child",
 			func() *tfObject {
 				obj := newTfObject()
-				obj.Set(map[string]any{"foo": &tfString{Unknown: true}, "bar": &tfBool{Val: false}})
+				obj.Set(map[string]any{"foo": &tfString{Unknown: true, Null: false, Val: ""}, "bar": &tfBool{Unknown: false, Null: false, Val: false}})
 
 				return obj
 			},
@@ -380,7 +380,7 @@ func TestTFObjectSliceGetAndValue(t *testing.T) {
 			"unknown",
 			func() *tfObjectSlice {
 				obj := newTfObject()
-				obj.Set(map[string]any{"foo": &tfString{Val: "foo"}, "bar": &tfBool{Val: false}})
+				obj.Set(map[string]any{"foo": &tfString{Unknown: false, Null: false, Val: "foo"}, "bar": &tfBool{Unknown: false, Null: false, Val: false}})
 				objs := newTfObjectSlice()
 				objs.AttrTypes = map[string]tftypes.Type{"foo": tftypes.String, "bar": tftypes.Bool}
 				objs.Set([]*tfObject{obj})
@@ -401,7 +401,7 @@ func TestTFObjectSliceGetAndValue(t *testing.T) {
 			"null",
 			func() *tfObjectSlice {
 				obj := newTfObject()
-				obj.Set(map[string]any{"foo": &tfString{Val: "foo"}, "bar": &tfBool{Val: false}})
+				obj.Set(map[string]any{"foo": &tfString{Unknown: false, Null: false, Val: "foo"}, "bar": &tfBool{Unknown: false, Null: false, Val: false}})
 				objs := newTfObjectSlice()
 				objs.AttrTypes = map[string]tftypes.Type{"foo": tftypes.String, "bar": tftypes.Bool}
 				objs.Set([]*tfObject{obj})
@@ -422,7 +422,7 @@ func TestTFObjectSliceGetAndValue(t *testing.T) {
 			"fully known",
 			func() *tfObjectSlice {
 				obj := newTfObject()
-				obj.Set(map[string]any{"foo": &tfString{Val: "foo"}, "bar": &tfBool{Val: false}})
+				obj.Set(map[string]any{"foo": &tfString{Unknown: false, Null: false, Val: "foo"}, "bar": &tfBool{Unknown: false, Null: false, Val: false}})
 				objs := newTfObjectSlice()
 				objs.AttrTypes = map[string]tftypes.Type{"foo": tftypes.String, "bar": tftypes.Bool}
 				objs.Set([]*tfObject{obj})
@@ -450,7 +450,7 @@ func TestTFObjectSliceGetAndValue(t *testing.T) {
 			"known with unknown child",
 			func() *tfObjectSlice {
 				obj := newTfObject()
-				obj.Set(map[string]any{"foo": &tfString{Unknown: true}, "bar": &tfBool{Val: false}})
+				obj.Set(map[string]any{"foo": &tfString{Unknown: true, Null: false, Val: ""}, "bar": &tfBool{Unknown: false, Null: false, Val: false}})
 				objs := newTfObjectSlice()
 				objs.AttrTypes = map[string]tftypes.Type{"foo": tftypes.String, "bar": tftypes.Bool}
 				objs.Set([]*tfObject{obj})
@@ -503,11 +503,11 @@ func TestDebugTfString(t *testing.T) {
 			"null",
 		},
 		{
-			&tfString{Unknown: true},
+			&tfString{Unknown: true, Null: false, Val: ""},
 			"unknown",
 		},
 		{
-			&tfString{Val: "bananas"},
+			&tfString{Unknown: false, Null: false, Val: "bananas"},
 			"bananas",
 		},
 	} {
@@ -527,11 +527,11 @@ func TestDebugTfNum(t *testing.T) {
 			"null",
 		},
 		{
-			&tfNum{Unknown: true},
+			&tfNum{Unknown: true, Null: false, Val: 0},
 			"unknown",
 		},
 		{
-			&tfNum{Val: 50},
+			&tfNum{Unknown: false, Null: false, Val: 50},
 			"50",
 		},
 	} {
@@ -551,15 +551,15 @@ func TestDebugTfBool(t *testing.T) {
 			"null",
 		},
 		{
-			&tfBool{Unknown: true},
+			&tfBool{Unknown: true, Null: false, Val: false},
 			"unknown",
 		},
 		{
-			&tfBool{Val: true},
+			&tfBool{Unknown: false, Null: false, Val: true},
 			"true",
 		},
 		{
-			&tfBool{Val: false},
+			&tfBool{Unknown: false, Null: false, Val: false},
 			"false",
 		},
 	} {
@@ -579,11 +579,11 @@ func TestDebugTfStringSlice(t *testing.T) {
 			"null",
 		},
 		{
-			&tfStringSlice{Unknown: true},
+			&tfStringSlice{Unknown: true, Null: false, Val: []*tfString{}},
 			"unknown",
 		},
 		{
-			&tfStringSlice{Val: []*tfString{{Val: "bananas"}, {Val: "apples"}, {Val: "mangos"}}},
+			&tfStringSlice{Unknown: false, Null: false, Val: []*tfString{{Unknown: false, Null: false, Val: "bananas"}, {Unknown: false, Null: false, Val: "apples"}, {Unknown: false, Null: false, Val: "mangos"}}},
 			"[bananas apples mangos]",
 		},
 	} {
@@ -609,7 +609,7 @@ func TestDebugTfStringMap(t *testing.T) {
 			"null",
 		},
 		{
-			&tfStringMap{Unknown: true},
+			&tfStringMap{Unknown: true, Null: false, Val: map[string]*tfString{}},
 			"unknown",
 		},
 		{
@@ -640,7 +640,7 @@ func TestDebugTfObject(t *testing.T) {
 			"null",
 		},
 		{
-			&tfObject{Unknown: true},
+			&tfObject{Unknown: true, Null: false, AttrTypes: map[string]tftypes.Type{}, Val: map[string]any{}, Optional: map[string]struct{}{}},
 			"unknown",
 		},
 		{
@@ -686,7 +686,7 @@ func TestDebugTfObjectSlice(t *testing.T) {
 			"null",
 		},
 		{
-			&tfObjectSlice{Unknown: true},
+			&tfObjectSlice{Unknown: true, Null: false, Val: []*tfObject{}, AttrTypes: map[string]tftypes.Type{}, Optional: map[string]struct{}{}},
 			"unknown",
 		},
 		{
